f6d960c2c3794f215f5bedf0ef1eebccb2b8c78c
kiranthilak <kiran.thilak@yahoo.com>
2022-09-16 00:47:45 +0530

    navbar lists added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,21 @@ import Icon from './Icon'
 const Navbar = () => {
   return (
     <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadlow-lg'>
-  
-       <div className='px-2 flex justify-between items-center w-full h-full'>
-        <div className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND</div>
-       </div>
+        <div className='px-2 flex justify-between items-center w-full h-full'>
+          <div className='flex items-center'>
+             <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND</h1>
+             <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Support</li>
+                <li>platforms</li>
+                <li>Pricing</li>
+             </ul>
+          </div>
+        </div>
 
 
-<Icon />
+         <Icon />
     </div>
   )
 }
