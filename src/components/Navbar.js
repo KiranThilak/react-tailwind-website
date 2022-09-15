cdbf1f8b73ebd99ce8875e22815e20d20b71394a
kiranthilak <kiran.thilak@yahoo.com>
2022-09-16 00:33:27 +0530

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Icon from './Icon'
+
 
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadlow-lg'>
+  
+       <div className='px-2 flex justify-between items-center w-full h-full'>
+        <div className='text-3xl font-bold mr-4 sm:text-4xl'>BRAND</div>
+       </div>
 
+
+<Icon />
     </div>
   )
 }
